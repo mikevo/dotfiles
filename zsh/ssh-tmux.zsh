8e0d8000f0c9b61c4084c60038dd38576d609e55
Michael Voetter <michael.voetter@ches.pro>
2025-08-26 08:41:41 +0200

    improve remote tmux autostart

--- zsh/ssh-tmux.zsh
+++ zsh/ssh-tmux.zsh
@@ -1,7 +1,8 @@
-if [[ -n $SSH_CONNECTION ]]; then
+if [[ -n $SSH_CONNECTION ]] && [[ -n $SSH_TTY ]]; then
     if which tmux >/dev/null 2>&1; then
         if [[ -z "$TMUX" ]]; then
             tmux new-session -A -s "ssh-session"
+            exit
         fi
     fi
 fi
