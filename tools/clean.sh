7dcbf2ac8105d9ba1060ae2f49b7078f10244b2f
Michael Voetter <mikevo@famv.net>
2025-08-26 16:04:57 +0000

    Update tools

--- tools/clean.sh
+++ tools/clean.sh
@@ -7,3 +7,11 @@ fi
 if [ -h "$HOME/.zshrc.old" ]; then
     unlink "$HOME/.zshrc.old"
 fi
+
+if [ -d "$HOME/.bash_it" ]; then
+    rm -rf "$HOME/.bash_it"
+fi
+
+if [ -d "$HOME/.zprezto" ]; then
+    rm -rf "$HOME/.zprezto"
+fi
