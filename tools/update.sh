ca12f8357dc54cf476c362e1ffa265703db8879d
Michael Voetter <mikevo@famv.net>
2025-08-26 20:24:53 +0000

    Update tools

--- tools/update.sh
+++ tools/update.sh
@@ -23,9 +23,5 @@ fi
 
 STOW_DIRS=".config/nvim .gnupg .local/bin .vim"
 for dir in $STOW_DIRS; do
-    if [ -h "$HOME/$dir" ]; then
-        unlink "$HOME/$dir"
-    fi
-    mkdir -p "$HOME/$dir"
-    touch "$HOME/$dir/.stow-keep"
+    rm -f "$HOME/$dir/.stow-keep"
 done
