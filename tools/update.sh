cb8e3e41c9d871f90456fd103f9009ec3297985a
Michael Voetter <mikevo@famv.net>
2025-08-26 19:50:23 +0000

    Update tools

--- tools/update.sh
+++ tools/update.sh
@@ -2,6 +2,11 @@
 
 DOTFILES="$HOME/.dotfiles"
 
+mkdir -p "$HOME/.config/nvim"
+mkdir -p "$HOME/.gnupg"
+mkdir -p "$HOME/.local/bin"
+mkdir -p "$HOME/.vim"
+
 if [ -d "$DOTFILES" ]; then
     cd "$DOTFILES"
     git pull
