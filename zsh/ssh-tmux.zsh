b1b23429722553123b8665646a92abe67e0ea62a
Michael Voetter <michael.voetter@ches.pro>
2025-08-27 10:14:46 +0200

    Use nvim when available

--- zsh/ssh-tmux.zsh
+++ zsh/ssh-tmux.zsh
@@ -1,5 +1,5 @@
 if [[ -n "$SSH_CONNECTION" ]] && [[ -n "$SSH_TTY" ]]; then
-    if which tmux >/dev/null 2>&1; then
+    if command -v tmux >/dev/null 2>&1; then
         if [[ -z "$TMUX" ]]; then
             tmux new-session -A -s "ssh-session"
             exit
